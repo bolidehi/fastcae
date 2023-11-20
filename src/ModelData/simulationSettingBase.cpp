6bfbcee63ea40c29d0e121699392011fe4d12e60
jdragon <jdragon01@sina.com>
2023-11-20 07:43:44 +0000

    改正拼写错误 SimlutationSetting改成SimulationSettingBase，这样xml文件中对应的标签名称也改变，便于解析xml文件
    Signed-off-by: jdragon <jdragon01@sina.com>

--- src/ModelData/simulationSettingBase.cpp
+++ src/ModelData/simulationSettingBase.cpp
@@ -1,29 +1,29 @@
-﻿#include "simulationSettingBase.h"
+#include "simulationSettingBase.h"
 #include "modelDataBase.h"
 #include <QDomElement>
 #include <QDomDocument>
 
 namespace ModelData
 {
-	SimlutationSettingBase::SimlutationSettingBase(ModelDataBase* m)
+	SimulationSettingBase::SimulationSettingBase(ModelDataBase* m)
 		:_modelData(m)
 	{
 		this->setModuleType(DataProperty::Module_Model);
 		this->setID(m->getID());
 	}
 
-	QDomElement& SimlutationSettingBase::writeToProjectFile(QDomDocument* doc, QDomElement* e)
+	QDomElement& SimulationSettingBase::writeToProjectFile(QDomDocument* doc, QDomElement* e)
 	{
 		DataProperty::DataBase::writeToProjectFile(doc, e);
 
 		return *e;
 	}
-	void SimlutationSettingBase::readDataFromProjectFile(QDomElement* e)
+	void SimulationSettingBase::readDataFromProjectFile(QDomElement* e)
 	{
 		DataProperty::DataBase::readDataFromProjectFile(e);
 	}
 
-	void SimlutationSettingBase::dataToStream(QDataStream* datas)
+	void SimulationSettingBase::dataToStream(QDataStream* datas)
 	{
 		DataProperty::DataBase::dataToStream(datas);
 	}
